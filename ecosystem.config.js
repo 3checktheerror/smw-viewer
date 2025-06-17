732fc9591850c0f7ddbb6fbc77e6014d984732ec
Qiu Junyi <checktheerror@gmail.com>
2025-06-17 14:49:50 +0800

    milestone: 信号收益接口初步

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   apps: [{
     name: 'smw-viewer',
-    script: '-m',
-    args: 'backend.app.tasks.task_manager',
+    script: 'backend/app/tasks/task_manager.py',
     cwd: __dirname,
     interpreter: './myenv/bin/python',
     interpreter_args: '-u',
@@ -11,10 +10,9 @@ module.exports = {
     },
   }, {
     name: 'smw-viewer-api',
-    script: 'backend.app.main:app',
-    args: '--host 0.0.0.0 --port 3004',
+    script: './myenv/bin/uvicorn',
+    args: 'backend.app.main:app --host 0.0.0.0 --port 3004',
     cwd: __dirname,
-    interpreter: './myenv/bin/uvicorn',
     env: {
       PYTHONPATH: '.',
     },
