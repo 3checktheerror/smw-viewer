9666c20c22173057579a376164095187c2cfacd9
Qiu Junyi <checktheerror@gmail.com>
2025-06-17 14:40:56 +0800

    milestone: 信号收益接口初步

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
     script: 'myenv/bin/python',
     args: '-m backend.app.tasks.task_manager',
     cwd: __dirname,
+    interpreter: 'python',
     interpreter_args: '-u',
     env: {
       PYTHONPATH: '.',
@@ -13,6 +14,7 @@ module.exports = {
     script: 'myenv/bin/uvicorn',
     args: 'backend.app.main:app --host 0.0.0.0 --port 3004',
     cwd: __dirname,
+    interpreter: 'python',
     interpreter_args: '-u',
     env: {
       PYTHONPATH: '.',
